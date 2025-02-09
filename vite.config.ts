04fa80502e303e5cb0765cb92939d932b6d7256f
Aye10032 <tc_aye@outlook.com>
2025-02-09 21:52:22 +0800

    添加字体

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
 import path from "path"
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import Unfonts from 'unplugin-fonts/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        Unfonts({
+            custom: {
+                families: [
+                    {
+                        name: 'Geist',
+                        src: './src/assets/fonts/geist/*.woff2',
+                    },
+                    {
+                        name: 'Geist Mono',
+                        src: './src/assets/fonts/geistmono/*.woff2',
+                    },
+                ],
+            },
+        })
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
@@ -17,7 +34,10 @@ export default defineConfig({
             // 字符串简写写法：
             // http://localhost:5173/foo
             // -> http://localhost:4567/foo
-            '/api': 'http://localhost:8000',
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true
+            },
         },
     },
 })
